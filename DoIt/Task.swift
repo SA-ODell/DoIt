487492b753a852e18958b540edbdd26e2bc7a9c0
SteveODell <saodell@mac.com>
2017-04-29 22:44:26 -0600

    Landing page

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Steve @work on 4/29/17.
+//  Copyright © 2017 Twenty-One-Bravo. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
